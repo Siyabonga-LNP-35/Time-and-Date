22aee56a255fa175cf0abc65b3c14e59e47fadbc
siyab <siyab@LAPTOP-GB2KUB0B>
2024-09-21 18:05:53 +0200

    first commit

---
+++ src/main/java/com/mycompany/timeanddate/TimeandDate.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.timeanddate;
+
+/**
+ *
+ * @author siyab
+ */
+public class TimeandDate {
+
+    public static void main(String[] args) {
+        
+    }
+}
